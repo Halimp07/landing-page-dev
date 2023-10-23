45330c4ef0ec244464b55808c03bac20666e148a
Halim <halimputra0701@gmail.com>
2023-10-23 17:27:00 +0700

    minor feat: faq dropdown

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -39,9 +39,12 @@ const Faq = () => {
                 Gimana caranya supaya orang-orang atau perusahaan percaya kalau
                 saya udah lulus dari EduSkill Bootcamp?
               </div>
-              <div
-              >
-                <img src={dropdown_btn} alt="dropdown-button" className="w-[19px] h-[9.5px]"/>
+              <div>
+                <img
+                  src={dropdown_btn}
+                  alt="dropdown-button"
+                  className="w-[19px] h-[9.5px]"
+                />
               </div>
             </div>
             {faq1Open && (
@@ -53,27 +56,103 @@ const Faq = () => {
                 Bootcamp.
               </div>
             )}
-            <div className="self-stretch px-6 py-3 bg-white rounded-lg shadow border border-teal-400 justify-between items-center inline-flex">
+            <div
+              className="justify-between px-6 py-3 bg-white rounded-lg shadow border border-teal-400  items-center inline-flex cursor-pointer"
+              onClick={toggleFaq1}
+            >
               <div className="h-7 text-emerald-800 text-base font-semibold font-['Poppins'] leading-normal tracking-tight">
-                Kapan agenda Bootcamp dimulai?
+                Kapan agenda Bootcamp dimulai?{" "}
+              </div>
+              <div>
+                <img
+                  src={dropdown_btn}
+                  alt="dropdown-button"
+                  className="w-[19px] h-[9.5px]"
+                />
               </div>
             </div>
-            <div className="self-stretch px-6 py-3 bg-white rounded-lg shadow border border-teal-400 justify-between items-center inline-flex">
+            {faq1Open && (
+              <div className=" px-6 py-3 text-emerald-800 text-base font-semibold font-['Poppins'] leading-normal tracking-tight mt-2">
+                {/* Display the answer when the FAQ is open */}
+                Ini adalah jawaban dari pertanyaan ini. Contoh: Orang-orang atau
+                perusahaan akan percaya jika Anda dapat menunjukkan proyek atau
+                portofolio yang sudah Anda buat setelah lulus dari EduSkill
+                Bootcamp.
+              </div>
+            )}
+            <div
+              className="justify-between px-6 py-3 bg-white rounded-lg shadow border border-teal-400  items-center inline-flex cursor-pointer"
+              onClick={toggleFaq1}
+            >
               <div className="h-7 text-emerald-800 text-base font-semibold font-['Poppins'] leading-normal tracking-tight">
                 Apakah programnya full online?
               </div>
+              <div>
+                <img
+                  src={dropdown_btn}
+                  alt="dropdown-button"
+                  className="w-[19px] h-[9.5px]"
+                />
+              </div>
             </div>
-            <div className="self-stretch px-6 py-3 bg-white rounded-lg shadow border border-teal-400 justify-between items-center inline-flex">
+            {faq1Open && (
+              <div className=" px-6 py-3 text-emerald-800 text-base font-semibold font-['Poppins'] leading-normal tracking-tight mt-2">
+                {/* Display the answer when the FAQ is open */}
+                Ini adalah jawaban dari pertanyaan ini. Contoh: Orang-orang atau
+                perusahaan akan percaya jika Anda dapat menunjukkan proyek atau
+                portofolio yang sudah Anda buat setelah lulus dari EduSkill
+                Bootcamp.
+              </div>
+            )}
+            <div
+              className="justify-between px-6 py-3 bg-white rounded-lg shadow border border-teal-400  items-center inline-flex cursor-pointer"
+              onClick={toggleFaq1}
+            >
               <div className="h-7 text-emerald-800 text-base font-semibold font-['Poppins'] leading-normal tracking-tight">
                 Apakah sedang ada promo potongan harga?
               </div>
+              <div>
+                <img
+                  src={dropdown_btn}
+                  alt="dropdown-button"
+                  className="w-[19px] h-[9.5px]"
+                />
+              </div>
             </div>
-            <div className="self-stretch px-6 py-3 bg-white rounded-lg shadow border border-teal-400 justify-between items-center inline-flex">
+            {faq1Open && (
+              <div className=" px-6 py-3 text-emerald-800 text-base font-semibold font-['Poppins'] leading-normal tracking-tight mt-2">
+                {/* Display the answer when the FAQ is open */}
+                Ini adalah jawaban dari pertanyaan ini. Contoh: Orang-orang atau
+                perusahaan akan percaya jika Anda dapat menunjukkan proyek atau
+                portofolio yang sudah Anda buat setelah lulus dari EduSkill
+                Bootcamp.
+              </div>
+            )}
+            <div
+              className="justify-between px-6 py-3 bg-white rounded-lg shadow border border-teal-400  items-center inline-flex cursor-pointer"
+              onClick={toggleFaq1}
+            >
               <div className="h-7 text-emerald-800 text-base font-semibold font-['Poppins'] leading-normal tracking-tight">
                 Kalau udah lulus dari Eduskill Bootcamp, bisa dibantu cari
                 kerja?
               </div>
+              <div>
+                <img
+                  src={dropdown_btn}
+                  alt="dropdown-button"
+                  className="w-[19px] h-[9.5px]"
+                />
+              </div>
             </div>
+            {faq1Open && (
+              <div className=" px-6 py-3 text-emerald-800 text-base font-semibold font-['Poppins'] leading-normal tracking-tight mt-2">
+                {/* Display the answer when the FAQ is open */}
+                Ini adalah jawaban dari pertanyaan ini. Contoh: Orang-orang atau
+                perusahaan akan percaya jika Anda dapat menunjukkan proyek atau
+                portofolio yang sudah Anda buat setelah lulus dari EduSkill
+                Bootcamp.
+              </div>
+            )}
           </div>
         </div>
       </div>
