57b9b95d75ca851f2b53ee1ae4b63e1e9b3fade2
Halim <halimputra0701@gmail.com>
2023-10-25 02:03:43 +0700

    major feat: add responsif benefit

--- src/pages/Benefit.jsx
+++ src/pages/Benefit.jsx
@@ -3,43 +3,43 @@ import { eduskill_left, consultant, prev, next } from "@/Data";
 
 const Benefit = () => {
   return (
-    <div className="w-full section bg-teal-400 lg:my-[50px] md:my-[50px] my-5 lg:mb-36 mb-14 lg:mx-auto md:mx-auto mx-0 relative">
+    <div className="w-full section bg-teal-400 lg:my-[50px] md:my-[50px] my-5 lg:mx-auto md:mx-auto mx-0 relative">
       <div className="w-full relative py-[40px] lg:pr-[67px] md:pr-[67px] pr-0 overflow-hidden">
-        {/* <div className="bg-teal-400 h-[25px] w-full rotate-[105deg] absolute z-20 left-[45%] top-1/2 transform -translate-x-1/2 -translate-y-1/2"></div> */}
-        {/* <div className="bg-orange-400 w-[100%] md:ml-[42%] xl:ml-[50%] absolute left-0 top-0 h-full -skew-x-[15deg] z-10"></div> */}
-        <div className=" text-white lg:text-[27px] md:text-[27px] text-base lg:flex md:flex lg:justify-end md:justify-end lg:text-start md:text-start text-center font-extrabold lg:mr-10 md:mr-10 mr-0 font-[''] capitalize leading-loose tracking-wide relative z-20">
+        <div className="lg:visible md:visible invisible bg-teal-400 h-[25px] w-full rotate-[105deg] absolute z-20 left-[45%] top-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="bg-orange-400 w-[100%] md:ml-[42%] xl:ml-[50%] absolute left-0 lg:top-0 md:top-0 bottom-0 lg:h-full md:full h-1/2 lg:-skew-x-[15deg] md:-skew-x-[15deg] -skew-x-0 z-10"></div>
+        <div className=" text-white lg:text-[27px] md:text-[27px] lg:mt-0 md:mt-0 mt-36 text-base lg:flex md:flex lg:justify-end md:justify-end lg:text-start md:text-start text-center font-extrabold lg:mr-10 md:mr-10 mr-0 font-[''] capitalize leading-loose tracking-wide relative z-20">
           Tenang, ada Eduskill Consultant!
         </div>
-
-        {/* {consultant.map((item) => (
-            <div
-              key={item.id}
-              className="flex justify-end space-x-3 relative z-20"
-            >
-              <div>
-                {item.textLabel && (
-                  <div className="text-right text-white text-lg font-normal font-['Inter'] capitalize leading-loose tracking-tight my-auto">
-                    {item.textLabel}
-                  </div>
-                )}
-              </div>
-              <div className="my-auto">
-                {item.additionalElements && (
-                  <div>
-                    {item.additionalElements.map((element) => (
-                      <div key={element.id}>
-                        <img
-                          src={element.icon}
-                          alt="Check Icon"
-                          className="w-7"
-                        />
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
+      
+        {consultant.map((item) => (
+          <div
+            key={item.id}
+            className="flex lg:justify-end md:justify-end justify-between lg:px-0 md:px-0 px-16 space-x-3 relative z-20"
+          >
+            <div>
+              {item.textLabel && (
+                <div className="lg:text-right md:text-right text-center text-white lg:text-lg md:text-lg text-sm font-normal font-['Inter'] capitalize leading-loose tracking-tight my-auto">
+                  {item.textLabel}
+                </div>
+              )}
             </div>
-          ))} */}
+            <div className="my-auto">
+              {item.additionalElements && (
+                <div>
+                  {item.additionalElements.map((element) => (
+                    <div key={element.id}>
+                      <img
+                        src={element.icon}
+                        alt="Check Icon"
+                        className="lg:w-7 md:w-7 w-5"
+                      />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
 
       <div className="bg-teal-400 absolute w-full top-0">
@@ -48,40 +48,40 @@ const Benefit = () => {
             <img className="w-20 h-6" src={eduskill_left} />
           </div>
 
-          <div className="flex-col justify-start items-start gap-1 lg:inline-flex md:inline-flex">
-            <div className="lg:w-96 md:w-96  h-36 flex-col justify-start items-start gap-1 lg:inline-flex md:inline-flex">
-              <div className="lg:w-0 md:w-0 w-[160px] lg:text-start text-center px-2 py-0.5 bg-orange-400 lg:mx-0 md:mx-0 mx-auto justify-center items-center gap-2.5 lg:inline-flex md:inline-flex">
+          <div className="flex-col lg:mt-0 md:mt-0 mt-10 justify-start items-start gap-1 lg:inline-flex md:inline-flex">
+            <div className="lg:w-96 md:w-96 w-44 h-36 flex-col lg:mx-0 md:mx-0 mx-auto justify-start items-start gap-1 lg:inline-flex md:inline-flex">
+              <div className="lg:text-start text-center py-0.5 bg-orange-400 lg:mx-0 md:mx-0 mx-auto justify-center items-center gap-2.5 lg:inline-flex md:inline-flex">
                 <div className="text-white lg:text-lg md:text-lg sm:text-center text-sm font-extrabold font-['Poppins'] capitalize leading-9 tracking-tight">
                   Mau ikut bootcamp
                 </div>
               </div>
-              <div className="lg:w-96 md:w-96 w-full h-28 lg:text-start md:text-start sm:text-center text-center text-white lg:text-4xl md:text-4xl text-base font-extrabold font-['Poppins'] capitalize leading-10 tracking-wide">
+              <div className="lg:w-96 md:w-96 w-full h-28 lg:text-start md:text-start sm:text-center text-center text-white lg:text-4xl md:text-4xl text-base font-extrabold font-['Poppins'] capitalize lg:leading-10 md:leading-10 leading-7 tracking-wide">
                 tapi takut salah <br />
                 pilih course?
               </div>
             </div>
           </div>
         </div>
-        {/* <div className="2xl:w-[1600px]  xl:w-[1324px] md:w-[1080px] w-full h-16 absolute flex justify-between items-center z-20 left-1/2 transform -translate-x-1/2 top-1/2">
-            <div className="w-16 h-16 absolute left-0">
-              <button>
+        <div className="2xl:w-[1600px] xl:w-[1324px] md:w-[1080px] w-full lg:h-16 md:h-16 h-44 absolute flex justify-between items-center z-20 left-1/2 transform -translate-x-1/2 top-1/2">
+          <div className="w-16 h-16 absolute left-0">
+            <button>
               <img
                 src={prev}
                 alt=""
-                className="w-16 h-16 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                className="lg:w-16 md:w-16 w-12 lg:h-16 md:h-16 h-12  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
               />
-              </button>
-            </div>
-            <div className="w-16 h-16 absolute right-0">
-              <button>
+            </button>
+          </div>
+          <div className="w-16 h-16 absolute right-0">
+            <button>
               <img
                 src={next}
                 alt=""
-                className="w-16 h-16 absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2"
+                className="lg:w-16 md:w-16 w-12 lg:h-16 md:h-16 h-12 absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2"
               />
-              </button>
-            </div>
-          </div> */}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
