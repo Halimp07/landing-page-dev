260274220a6b801786d02b5272c049978637db2d
Halim <halimputra0701@gmail.com>
2023-10-24 17:57:48 +0700

    minor test: testing responsive

--- src/pages/Eduskill.jsx
+++ src/pages/Eduskill.jsx
@@ -6,16 +6,16 @@ const Eduskill = () => {
     <div className="w-full h-[592px] bg-[#B0E4DE]/40 ">
       <div className="container mx-auto ">
         <div className="lg:flex md:flex justify-between md:space-x-4">
-          <div className="section-1 lg:mt-[68px] md:mt-[68px] mt-0 lg:pt-0 md:pt-0 pt-2 mx-4">
-            <div className="logo-eduskill flex lg:justify-start md:justify-start justify-start">
+          <div className="section-1 lg:mt-[68px] md:mt-[68px] mt-0 lg:pt-0 md:pt-0 pt-2 lg:mx-0 md:mx-0 mx-[20px]">
+            <div className="logo-eduskill flex lg:justify-start md:justify-start justify-start relative">
               <img
                 src={eduskill}
                 alt="logo_eduskill"
-                className="lg:w-52 md:w-52 w-44 lg:h-16 md:h-16 h-16"
+                className="lg:w-48 md:w-52 w-36 lg:h-16 md:h-16 h-12"
               />
             </div>
 
-            <div className="lg:w-[603px] md:w-[603px] w-full mt-3 lg:text-start md:text-start text-start">
+            <div className="lg:w-[603px] md:w-[603px] w-full lg:mt-3 md:mt-3 mt-2 lg:text-start md:text-start text-start">
               <span className="text-black lg:text-6xl md:text-6xl text-lg font-extrabold font-[''] capitalize tracking-wide">
                 Upgrade Skill Pemrogramanmu Bersama{" "}
               </span>
@@ -23,15 +23,14 @@ const Eduskill = () => {
                 Eduskill
               </span>
             </div>
-            <div className="mt-5 text-justify">
-              <span className="text-black text-base font-normal font-[''] lg:leading-loose md:leading-loose leading-none lg:tracking-tight">
-                Jadi
+            <div className="lg:mt-5 md:mt-5 mt-2 text-justify">
+              <span className="text-black text-base font-normal font-[''] lg:leading-loose md:leading-loose leading-none lg:tracking-tight md:tracking-tight tracking-normal">
+                Jadi{" "}
               </span>
-              <span className="text-black text-base font-bold font-[''] lg:leading-loose md:leading-loose leading-none lg:tracking-tight">
-                {" "}
+              <span className="text-black text-base font-bold font-[''] lg:leading-loose md:leading-loose leading-none lg:tracking-tight md:tracking-tight tracking-normal">
                 #SelangkahdiDepan
               </span>
-              <span className="text-black text-base font-normal font-[''] lg:leading-loose md:leading-loose leading-none tracking-tight">
+              <span className="text-black text-base font-normal font-[''] lg:leading-loose md:leading-loose leading-none lg:tracking-tight md:tracking-tight tracking-normal">
                 {" "}
                 kuasai digital skills dengan metode belajar yang fun!
                 <br />
@@ -39,8 +38,8 @@ const Eduskill = () => {
                 kerja
               </span>
             </div>
-            <div className="mt-6">
-              <div className="w-52 h-14 px-6 lg:py-3 md:py-3 py-2 bg-orange-400 rounded-xl justify-center items-center gap-2.5 inline-flex">
+            <div className="lg:mt-6 md:mt-6 mt-3">
+              <div className="lg:w-52 md:w-52 w-40 h-14 px-6 lg:py-3 md:py-3 py-2 bg-orange-400 rounded-xl justify-center items-center gap-2.5 inline-flex">
                 <div className="text-center text-white lg:text-lg md:text-lg text-base font-semibold font-[''] leading-9 tracking-tight">
                   <a href="http://linktr.ee/eduskill">Mulai Sekarang!</a>
                 </div>
