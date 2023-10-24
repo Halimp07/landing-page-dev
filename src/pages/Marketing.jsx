dc8eee2bac5eee718791963d67ee1b8226483b94
Halim <halimputra0701@gmail.com>
2023-10-24 22:29:44 +0700

    minor feat: add responsive marketing

--- src/pages/Marketing.jsx
+++ src/pages/Marketing.jsx
@@ -5,7 +5,7 @@ const Marketing = () => {
     <div className="lg:mb-[50px] md:mb-[50px] mb-[30px] lg:flex md:flex justify-center lg:mx-0 md:mx-0 mx-[20px]">
       <div className="lg:section md:section">
         <div className="lg:flex md:flex">
-          <div className="w-full lg:h-72 md:h-72 h-56 flex-col justify-start items-start lg:gap-6 md:gap-6 gap-3 inline-flex">
+          <div className="w-full lg:h-72 md:h-72 h-full flex-col justify-start items-start lg:gap-6 md:gap-6 gap-3 inline-flex">
             <div className="lg:mr-20 md:mr-20 mr-14 text-orange-500 lg:text-3xl md:text-3xl text-lg font-bold font-['Poppins'] lg:leading-10 md:leading-10 leading-1 lg:tracking-wide md:tracking-wide tracking-normal">
               Mengapa Eduskill menjadi Pilihan Terbaik untuk Upskilling?
             </div>
@@ -19,7 +19,7 @@ const Marketing = () => {
             </div>
           </div>
 
-          <div className="w-full flex-col lg:gap-6 md:gap-6 gap-3 inline-flex items-start lg:mt-0 md:mt-0 mt-4">
+          <div className="w-full flex-col lg:gap-6 md:gap-6 gap-3 inline-flex items-start lg:mt-0 md:mt-0 mt-8">
             <div className="w-full lg:h-40 md:h-40 h-32 lg:px-4 md:px-4 px-3 py-9 bg-white rounded-2xl border border-neutral-300 justify-start items-center inline-flex">
               <div className="lg:w-16 md:w-16 w-14 lg:h-16 md:h-16 h-12 lg:mr-[25px] md:mr-[25px] mr-3 bg-teal-500 rounded-full" />
               <div className="flex-col justify-start items-st4art gap-1 inline-flex">
