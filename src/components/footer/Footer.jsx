1d5d3447954cda5a99d217955c420c5572ab7e06
Halim <halimputra0701@gmail.com>
2023-10-24 23:01:48 +0700

    minor style: change font size footer contact

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
             src={bg_overlay}
           />{" "}
         <div className="lg:h-72 md:h-72 h-56 xpy-12 flex-col justify-center items-center gap-6 inline-flex">
-        <div className="lg:w-[830px] md:w-[830px] w-full text-center text-white lg:text-4xl md:text-4xl text-base font-bold font-['Poppins'] lg:leading-10 md:leading-10 leading-7 tracking-wide relative">
+        <div className="lg:w-[830px] md:w-[830px] w-[300px] text-center text-white lg:text-4xl md:text-4xl text-base font-bold font-['Poppins'] lg:leading-10 md:leading-10 leading-7 tracking-wide relative">
               Yuk, Jadi Talenta Handal di Industri Digital Sekarang!{" "}
             </div>
             <div className="px-6 lg:py-3 md:py-3 py-2 bg-teal-600 rounded-xl justify-center items-center lg:gap-2 md:gap-2 gap-0 inline-flex relative">
