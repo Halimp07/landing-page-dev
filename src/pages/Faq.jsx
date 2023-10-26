a260f6d3db0180f69cf0a8d4d00901a2b53bd25e
Halim <halimputra0701@gmail.com>
2023-10-26 21:51:51 +0700

    major feat: adding animation in faq

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { faq, dropdown_btn, dropup_btn } from "@/Data";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const FaqItem = ({ item, toggleFaq, isItemOpen }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   const { id, question, answer } = item;
+
+  const aosProps =
+    id === 0 || id === 2 || id === 4
+      ? {
+          "data-aos": "fade-right",
+          "data-aos-offset": "300",
+          "data-aos-easing": "ease-in-sine",
+        }
+      : {
+          "data-aos": "fade-left",
+          "data-aos-offset": "300",
+          "data-aos-easing": "ease-in-sine",
+        };
 
   return (
     <div
+      {...aosProps}
       className="justify-between lg:px-6 md:px-6 px-8 lg:py-3 md:py-3 py-4 bg-white rounded-lg shadow border border-teal-400 items-center inline-flex cursor-pointer"
       onClick={toggleFaq}
     >
@@ -38,14 +58,23 @@ const Faq = () => {
     <div className="w-full flex justify-center">
       <div className="lg:section md:section lg:my-[50px] md:my-[50px] my-5 flex-col inline-flex items-center">
         {faq.map((item) => (
-          <div key={item.id} className="lg:mb-[25px] md:mb-[25px] mb-4 flex-col lg:w-full md:w-full inline-flex">
+          <div
+            key={item.id}
+            className="lg:mb-[25px] md:mb-[25px] mb-4 flex-col lg:w-full md:w-full inline-flex"
+          >
             <FaqItem
               item={item}
               toggleFaq={() => toggleFaq(item.id)}
               isItemOpen={openFaqIds.includes(item.id)}
             />
             {openFaqIds.includes(item.id) && (
-              <div className="w-full h-32 lg:px-6 md:px-6 px-8 bg-emerald-50 rounded-tr rounded-br border-l-2 border-teal-700 justify-center items-center inline-flex">
+              <div
+              data-aos="fade-zoom-in"
+              data-aos-easing="ease-in-back"
+              data-aos-delay="50"
+              data-aos-offset="0"
+                className="w-full h-32 lg:px-6 md:px-6 px-8 bg-emerald-50 rounded-tr rounded-br border-l-2 border-teal-700 justify-center items-center inline-flex"
+              >
                 <div className="lg:w-full md:w-full w-full text-black lg:text-base md:text-base text-[11px] font-normal font-['Poppins'] leading-normal tracking-tight">
                   {item.answer}
                 </div>
