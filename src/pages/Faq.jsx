71508c860b28298db908d4d79df1429e547b7064
Halim <halimputra0701@gmail.com>
2023-11-03 18:51:27 +0700

    minor feat: mx benefit layout

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -57,32 +57,27 @@ const Faq = () => {
   return (
     <div className="w-full flex justify-center">
       <div className="lg:section md:section lg:my-[50px] md:my-[50px] my-5 flex-col inline-flex items-center">
-        
-        {faq.map((item) => (
-          <div
-            key={item.id}
-            className="lg:mb-[25px] md:mb-[15px] mb-4 flex-col lg:w-full md:w-full inline-flex"
-          >
-            <FaqItem
-              item={item}
-              toggleFaq={() => toggleFaq(item.id)}
-              isItemOpen={openFaqIds.includes(item.id)}
-            />
-            {openFaqIds.includes(item.id) && (
-              <div
-              // data-aos="fade-zoom-in"
-              // data-aos-easing="ease-in-back"
-              // data-aos-delay="50"
-              // data-aos-offset="0"
-                className="w-full lg:h-20 md:h-20 h-[5rem] lg:px-6 md:px-8 px-8 bg-emerald-50 rounded-tr rounded-br border-l-2 border-teal-700 justify-center items-center inline-flex"
-              >
-                <div className="lg:w-full md:w-full w-[290px] text-black 2xl:text-base xl:text-base lg:text-sm md:text-sm text-[11px] font-normal font-['Poppins'] leading-normal tracking-tight">
-                  {item.answer}
+        <div className="2xl:mx-0 xl:mx-10 lg:mx-0 md:mx-0 mx-5">
+          {faq.map((item) => (
+            <div
+              key={item.id}
+              className="lg:mb-[25px] md:mb-[15px] mb-4 flex-col lg:w-full md:w-full inline-flex"
+            >
+              <FaqItem  
+                item={item}
+                toggleFaq={() => toggleFaq(item.id)}
+                isItemOpen={openFaqIds.includes(item.id)}
+              />
+              {openFaqIds.includes(item.id) && (
+                <div className="w-full lg:h-20 md:h-20 h-[5rem] lg:px-6 md:px-8 px-8 bg-emerald-50 rounded-tr rounded-br border-l-2 border-teal-700 justify-center items-center inline-flex">
+                  <div className="lg:w-full md:w-full w-[290px] text-black 2xl:text-base xl:text-base lg:text-sm md:text-sm text-[11px] font-normal font-['Poppins'] leading-normal tracking-tight">
+                    {item.answer}
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        ))}
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
